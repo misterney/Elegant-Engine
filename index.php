bfcaf09a0e5b4390725e56970b1ac1ee5c05f8d0
misterney <mister-ney@Mail.ru>
2015-07-15 23:45:43 +0500

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,17 @@
 
 	define('ENGINE_PATH', 			realpath(dirname(__FILE__)) . '/');
 	define('ENGINE_URL',  			'http://' . $_SERVER['SERVER_NAME'] . $url);
-	define('ENGINE_PATH_APPLICATION', 		ENGINE_PATH.'application/');
-	define('ENGINE_PATH_SYS_MODULES', 		ENGINE_PATH_APPLICATION.'sys-modules/');
-	define('ENGINE_PATH_RES_MODULES',		ENGINE_PATH_APPLICATION.'res-modules/');
+	define('ENGINE_PATH_APPLICATION', 	ENGINE_PATH.'application/');
+	define('ENGINE_PATH_SYS_MODULES', 	ENGINE_PATH_APPLICATION.'sys-modules/');
+	define('ENGINE_PATH_RES_MODULES',	ENGINE_PATH_APPLICATION.'res-modules/');
 	define('ENGINE_PATH_CLASSES', 		ENGINE_PATH_APPLICATION.'classes/');
-	define('ENGINE_PATH_MVC', 			ENGINE_PATH.'mvc/');
+	define('ENGINE_PATH_MVC', 		ENGINE_PATH.'mvc/');
 	define('ENGINE_PATH_CONFIGS',  		ENGINE_PATH_APPLICATION.'configs/');
-	define('ENGINE_PATH_LIBRARIES', 		ENGINE_PATH_APPLICATION.'libraries/');
+	define('ENGINE_PATH_LIBRARIES', 	ENGINE_PATH_APPLICATION.'libraries/');
 	define('SEAL_CHECK',  			TRUE);
 	define('URL_ASSETS',  			ENGINE_URL.'/assets/');
-	define('URL_JS',  				ENGINE_URL.'/assets/js/');
-	define('URL_CSS',  				ENGINE_URL.'/assets/css/');
+	define('URL_JS',  			ENGINE_URL.'/assets/js/');
+	define('URL_CSS',  			ENGINE_URL.'/assets/css/');
 	define('URL_IMAGES',  			ENGINE_URL.'/assets/images/');
 
 	include_once(ENGINE_PATH_APPLICATION.'/classes/autoloader.class.php');
